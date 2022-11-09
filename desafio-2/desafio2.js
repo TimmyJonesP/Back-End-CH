dbe3424c23ed7e6471d25f58ac0d131532cceb67
Sandro <sandrocarioli43@gmail.com>
2022-11-09 19:02:37 -0300

    desafio2

--- desafio-2/desafio2.js
+++ desafio-2/desafio2.js
@@ -1,50 +1,133 @@
-const fs = require('fs');
+const fs = require("fs");
 
 class Contenedor {
+    productArray = new Array();
+    typeId;
 
-    arrayObj = [];
-    id = this.arrayObj.length
-    
-    constructor(fileName){
+
+    constructor(fileName) {
         this.fileName = fileName;
-        if(fs.existsSync(fileName)){
-            this.arrayObj = JSON.parse(fs.readFileSync(this.fileName, "utf-8"));
-            console.log("exists")
-        } else{
-            console.log("it doesn't exists")
 
+        if (fs.existsSync(fileName)) {
+            ///si existe el .txt, lo trae. Segun el largo del array, asigna nuevo ID al objeto con el "#", y aclara que se existe el archiv.
+            this.productArray = JSON.parse(fs.readFileSync(this.fileName, "utf-8"));
+            this.typeId = this.#detId();
+            console.log("Archivo Existente");
+        } else {
+            ///asigna a 0 el primer ID y crea el nuevo archivo con el nombre asignado en el "new Contenedor("")"
+            this.typeId = 0;
+            fs.writeFileSync(this.fileName, JSON.stringify(this.productArray));
+            console.log("Archivo no encontrado... se creará.");
+        }
+    };
+
+    ///asignador de ID 
+    #detId() {
+        if (this.productArray.length > 0) {
+            let riseId = this.productArray.reduce((acc, item) => {
+                return Math.max(acc, item.id)
+            }, 0)
+            return riseId + 1;
+        } else {
+            return 0;
+        }
+    };
+
+    async save(Object) {
+        try {
+            if (!this.#thisIs(Object)) {
+                Object["id"] = this.typeId + 1;
+                this.typeId++;
+                this.productArray.push(Object);
+                ///escribe en el archivo el objeto con ID asignado.
+                await fs.promises.writeFile(this.fileName, JSON.stringify(this.productArray));
+                console.log(`Se guardó el libro n° ${Object.id}`);
+                return Promise.resolve(Object.id);
+            }
+            else {
+                console.log("Se han guardado los cambios");
+            }
+        }
+        catch (error) {
+            console.log(error);
         }
     }
-    async save(object){
-            object[id] = this.id;
-            this.id++;
-            this.arrayObj.push(object)
-            await fs.promises.writeFile(this.fileName, JSON.stringify(this.arrayObj));
-                console.log("uploaded" + object.id);
-                return Promise.resolve(object.id);
+
+    #thisIs(a) {
+        let resp = false;
+        this.productArray.filter(e => {
+            if (e.title == a.title && e.price == a.price && e.img == a.img) {
+                resp = true;
             }
-            catch (err){    
-                console.log(err)
-            }
-    getById(reqId){
-        obj = null
-        this.arrayObj.map((o) => {
-            if(o.id === reqId){
-                obj = o
-                console.log(obj)
-            } else{
-                console.log(obj)
-            }
-        })
+        });
+        return resp;
     }
-    getAll(){
-        try{
-            const data = fs.promises.readFile(this.fileName, "utf-8")
-            this.arrayObj = JSON.parse(data)
-            console.log(this.arrayObj)
-        }catch(err){
-            console.log(err)
+
+    getById(id) {
+        ///retorna el encontrado por id ý sino un "null"
+        return this.productArray.find(p => p.id == id) || null
+    }
+
+    async getAll() {
+        try {
+            ///lectura, asignación y vista de todos los objetos dentro del file.
+            const data = await fs.promises.readFile(this.fileName, "utf-8");
+            this.productArray = JSON.parse(data);
+            console.log(this.productArray)
+        }catch (err) {
+            console.log(err);
         }
     }
-    deleteById = (reqId) => this.arrayObj.filter(o => o.id !== reqId);
+
+    async deleteById(id) {
+        ///Limpia la coincidencia
+        this.productArray = this.productArray.filter(p => p.id !== id)
+
+        try {
+            ///la escribe en el archivo como es el nuevo array.-
+            await fs.promises.writeFile(this.fileName, JSON.stringify(this.productArray));
+            return { id }
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    async deleteAll() {
+        ///envía un array vacío y luego lo escribe en la ruta correspondiente.
+        this.productArray = [];
+        try {
+            await fs.promises.writeFile(this.fileName, JSON.stringify(this.productArray))  //escribe en un archivo
+            console.log("Todo borrado");
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
 }
+
+
+
+///pruebas
+let libro1 = { title: "El señor de los anillos: La comunidad del anillo", price: 150, img: "./producto1.jpg" };
+let libro2 = { title: "El señor de los anillos: Las dos torres", price: 200, img: "./producto2.jpg" };
+let libro3 = { title: "El señor de los anillos: El retorno del rey", price: 250, img: "./producto3.jpg" };
+
+///asignacion de ruta
+const books = new Contenedor("./productos.txt");
+
+///test run
+
+action = () => {
+
+    books.getAll()
+        .then(() => books.save(libro1))
+        .then(() => books.save(libro2))
+        .then(() => books.save(libro3))
+        .then(() => {
+            console.log(books.getById(1));
+        })
+}
+///exe
+action();
